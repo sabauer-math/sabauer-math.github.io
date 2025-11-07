03c1a618e8f8186abb08b6b23e817a020aa9a106
sabauer-math <sabauer.math@gmail.com>
2025-11-07 11:06:59 +0000

    Deploying to gh-pages from @ sabauer-math/sabauer-math.github.io@4d12434da4a2a4944f8655bb3ff920e77cdfe94a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "sabauer-math/samuelbauer.github.io",
+    "data-repo": "sabauer-math/sabauer-math.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
